08220f38b1ae63241e0267b08e80ef5f0cc15d1e
AndrewGnagy <Andrew.Gnagy@genesys.com>
2019-02-16 11:30:25 -0500

    Sticky comment the new submission

--- zachlowebot.py
+++ zachlowebot.py
@@ -25,9 +25,9 @@ assert 'ESPN' in browser.title
 # Grab links
 links = browser.find_elements_by_css_selector('#main-container table.gsc-table-result a.gs-title')
 
-# I'll just look at the first 4 to keep it recent
+# I'll just look at the first 3 to keep it recent
 valid_links = []
-for i, a in enumerate(links[:4]):
+for i, a in enumerate(links[:3]):
     print(i, a.text, a.get_attribute("href"))
     # Only stories, no dumb videos
     if ("/story/" in a.get_attribute("href")):
@@ -49,9 +49,11 @@ for i, submission in enumerate(subreddit.new(limit=25)):
 links_to_post = [new_link for new_link in valid_links if valid_and_not_already_posted(new_link['url'], existing_links)]
 print(links_to_post)
 
+# Submit the posts
 for link in links_to_post:
-    subreddit.submit(link['title'], url=link['url'])
+    submission = subreddit.submit(link['title'], url=link['url'])
+    # Wait 5 seconds in case Reddit api is slow
     time.sleep(5)
-    for submission in subreddit.new(limit=10):
-        if submission.title == link.title:
-            submission.reply('This post was generated automatically by ZachLoweBot')
+    # Comment on newly posted submission
+    comment = submission.reply('This post was generated automatically by ZachLoweBot')
+    comment.mod.distinguish(sticky=True)
